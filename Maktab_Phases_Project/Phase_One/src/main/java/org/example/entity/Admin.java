0a5a7f5d5c79c3515e5f4904ce06d4d323b6d077
amrialiPeymani <piliconwalys@gmail.com>
2023-10-08 00:58:31 +0330

    fields: name, lastName, email, password

---
+++ Maktab_Phases_Project/Phase_One/src/main/java/org/example/entity/Admin.java
@@ -0,0 +1,33 @@
+package org.example.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name= "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name= "lastName", nullable = false)
+    private String lastName;
+
+    @Column(name= "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name= "password", nullable = false)
+    private String password;
+
+}
