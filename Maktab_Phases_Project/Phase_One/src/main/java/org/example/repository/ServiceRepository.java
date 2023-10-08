4eaa64a08eadcc286cefd6fb0def7dc8de96d117
amrialiPeymani <piliconwalys@gmail.com>
2023-10-08 17:32:36 +0330

    CRUD method's

--- Maktab_Phases_Project/Phase_One/src/main/java/org/example/repository/ServiceRepository.java
+++ Maktab_Phases_Project/Phase_One/src/main/java/org/example/repository/ServiceRepository.java
@@ -1,4 +1,18 @@
 package org.example.repository;
 
+import org.example.entity.Service;
+
+import java.util.List;
+
 public interface ServiceRepository {
+
+    void saveService(Service service);
+
+    Service getServiceById(Long id);
+
+    List<Service> getAllServices();
+
+    void updateService(Service service);
+
+    void deleteService(Service service);
 }
