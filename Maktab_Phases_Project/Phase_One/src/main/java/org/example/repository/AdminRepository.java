59cdc00c002c0548b69ceb93b2b4ac11eed90597
amrialiPeymani <piliconwalys@gmail.com>
2023-10-08 17:31:51 +0330

    wrote the getById and update method of the admin

--- Maktab_Phases_Project/Phase_One/src/main/java/org/example/repository/AdminRepository.java
+++ Maktab_Phases_Project/Phase_One/src/main/java/org/example/repository/AdminRepository.java
@@ -1,4 +1,11 @@
 package org.example.repository;
 
+import org.example.entity.Admin;
+
 public interface AdminRepository {
+
+    Admin getAdminById(long id);
+
+    void updateAdmin(Admin admin);
+
 }
