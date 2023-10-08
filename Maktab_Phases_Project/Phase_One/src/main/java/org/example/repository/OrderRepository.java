130d70ca4a7f4b6407e7a6ea5b2c742b92a356ef
amrialiPeymani <piliconwalys@gmail.com>
2023-10-08 17:32:21 +0330

    CRUD method's

--- Maktab_Phases_Project/Phase_One/src/main/java/org/example/repository/OrderRepository.java
+++ Maktab_Phases_Project/Phase_One/src/main/java/org/example/repository/OrderRepository.java
@@ -1,4 +1,19 @@
 package org.example.repository;
 
+import org.example.entity.Order;
+
+import java.util.List;
+
 public interface OrderRepository {
+
+    void saveOrder(Order order);
+
+    Order getOrderById(Long id);
+
+    List<Order> getAllOrder();
+
+    void updateOrder(Order order);
+
+    void deleteOrder(Order order);
+
 }
