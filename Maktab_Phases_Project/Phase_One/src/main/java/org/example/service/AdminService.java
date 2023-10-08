3afb1e80d3f46f9db28c37d433415e0652d6e3cf
amrialiPeymani <piliconwalys@gmail.com>
2023-10-08 18:45:53 +0330

    update and getById admin

--- Maktab_Phases_Project/Phase_One/src/main/java/org/example/service/AdminService.java
+++ Maktab_Phases_Project/Phase_One/src/main/java/org/example/service/AdminService.java
@@ -1,4 +1,10 @@
 package org.example.service;
 
+import org.example.entity.Admin;
+
 public interface AdminService {
-        }
+
+        Admin getAdminById(long id);
+
+        void updateAdmin(Admin admin);
+}
--- Maktab_Phases_Project/Phase_One/src/main/java/org/example/service/Impl/AdminServiceImpl.java
+++ Maktab_Phases_Project/Phase_One/src/main/java/org/example/service/Impl/AdminServiceImpl.java
@@ -3,22 +3,23 @@ package org.example.service.Impl;
 import org.example.base.BaseRepository;
 import org.example.entity.Admin;
 import org.example.repository.AdminRepository;
+import org.example.repository.Impl.AdminRepositoryImpl;
+import org.example.service.AdminService;
 
-public class AdminServiceImpl extends BaseRepository implements AdminRepository {
+public class AdminServiceImpl  implements AdminService {
+
+    private final AdminRepository adminRepository;
+
+    public AdminServiceImpl(AdminRepository adminRepository){
+        this.adminRepository = adminRepository;
+    }
     @Override
     public Admin getAdminById(long id) {
-        return em.find(Admin.class, id);
+        return adminRepository.getAdminById(id);
     }
 
     @Override
     public void updateAdmin(Admin admin) {
-        try {
-            em.getTransaction().begin();
-            em.merge(admin);
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            em.getTransaction().rollback();
-            throw e;
-        }
+        adminRepository.updateAdmin(admin);
     }
 }
