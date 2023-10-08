d1b8100e91ca32dc96de84dde9baab596e425de2
amrialiPeymani <piliconwalys@gmail.com>
2023-10-08 17:32:17 +0330

    CRUD method's

--- Maktab_Phases_Project/Phase_One/src/main/java/org/example/repository/CustomerRepository.java
+++ Maktab_Phases_Project/Phase_One/src/main/java/org/example/repository/CustomerRepository.java
@@ -1,4 +1,18 @@
 package org.example.repository;
 
+import org.example.entity.Customer;
+
+import java.util.List;
+
 public interface CustomerRepository {
+
+    void saveCustomer(Customer customer);
+
+    Customer getCustomerById(Long id);
+
+    List<Customer> getAllCustomer();
+
+    void updateCustomer(Customer customer);
+
+    void deleteCustomer(Customer customer);
 }
