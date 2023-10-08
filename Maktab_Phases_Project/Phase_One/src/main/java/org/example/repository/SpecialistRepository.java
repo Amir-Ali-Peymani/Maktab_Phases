04baec55171a82e34515d0b982f3e703dde1c1f9
amrialiPeymani <piliconwalys@gmail.com>
2023-10-08 17:32:42 +0330

    CRUD method's

--- Maktab_Phases_Project/Phase_One/src/main/java/org/example/repository/SpecialistRepository.java
+++ Maktab_Phases_Project/Phase_One/src/main/java/org/example/repository/SpecialistRepository.java
@@ -1,4 +1,18 @@
 package org.example.repository;
 
+import org.example.entity.Specialist;
+
+import java.util.List;
+
 public interface SpecialistRepository {
+
+    void saveSpecialist(Specialist specialist);
+
+    Specialist getSpecialistById(Long id);
+
+    List<Specialist> getAllSpecialists();
+
+    void updateSpecialist(Specialist specialist);
+
+    void deleteSpecialist(Specialist specialist);
 }
