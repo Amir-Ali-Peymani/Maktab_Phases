d8f91acb9382fa9ef930eeb9eb3251892f513062
amrialiPeymani <piliconwalys@gmail.com>
2023-10-08 18:19:21 +0330

    interface and the class Impl classes

---
+++ Maktab_Phases_Project/Phase_One/src/main/java/org/example/service/Impl/AdminServiceImpl.java
@@ -0,0 +1,24 @@
+package org.example.service.Impl;
+
+import org.example.base.BaseRepository;
+import org.example.entity.Admin;
+import org.example.repository.AdminRepository;
+
+public class AdminServiceImpl extends BaseRepository implements AdminRepository {
+    @Override
+    public Admin getAdminById(long id) {
+        return em.find(Admin.class, id);
+    }
+
+    @Override
+    public void updateAdmin(Admin admin) {
+        try {
+            em.getTransaction().begin();
+            em.merge(admin);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw e;
+        }
+    }
+}
