85c5587c0afb839090453f80aff5d40bdb00c23c
amrialiPeymani <piliconwalys@gmail.com>
2023-10-08 18:19:54 +0330

    interface and the class Impl classes

---
+++ Maktab_Phases_Project/Phase_One/src/main/java/org/example/service/Impl/SubServiceServiceImpl.java
@@ -0,0 +1,55 @@
+package org.example.service.Impl;
+
+import org.example.base.BaseRepository;
+import org.example.entity.SubService;
+import org.example.repository.SubServiceRepository;
+
+import java.util.List;
+
+public class SubServiceServiceImpl extends BaseRepository implements SubServiceRepository {
+    @Override
+    public void saveSubService(SubService subService) {
+        try {
+            em.getTransaction().begin();
+            em.persist(subService);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw e;
+        }
+    }
+
+    @Override
+    public SubService getSubServiceById(Long id) {
+        return em.find(SubService.class, id);
+    }
+
+    @Override
+    public List<SubService> getAllSubServices() {
+        return em.createQuery("SELECT s FROM SubService  s", SubService.class).getResultList();
+    }
+
+    @Override
+    public void updateSubService(SubService subService) {
+        try {
+            em.getTransaction().begin();
+            em.merge(subService);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw e;
+        }
+    }
+
+    @Override
+    public void deleteSubService(SubService subService) {
+        try {
+            em.getTransaction().begin();
+            em.remove(em.contains(subService) ? subService : em.merge(subService));
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw e;
+        }
+    }
+}
---
+++ Maktab_Phases_Project/Phase_One/src/main/java/org/example/service/SubServiceService.java
@@ -0,0 +1,18 @@
+package org.example.service;
+
+import org.example.entity.SubService;
+
+import java.util.List;
+
+public interface SubServiceService {
+
+    void saveSubService(SubService subService);
+
+    SubService getSubServiceById(Long id);
+
+    List<SubService> getAllSubServices();
+
+    void updateSubService(SubService subService);
+
+    void deleteSubService(SubService subService);
+}
