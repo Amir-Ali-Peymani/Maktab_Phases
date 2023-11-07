d3f630b15f8bb28aaa90d0e782c142c4ef58a663
amrialiPeymani <piliconwalys@gmail.com>
2023-11-07 16:41:52 +0330

    edit CustomerService.java with throwing exception

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/CustomerService.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/CustomerService.java
@@ -1,7 +1,8 @@
 package com.example.phase3.service;
 
 import com.example.phase3.entity.Customer;
-import com.example.phase3.exception.AuthenticationException;
+import com.example.phase3.exception.*;
+import com.example.phase3.exception.NullPointerException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,13 +10,13 @@ import java.util.List;
 @Service
 public interface CustomerService {
 
-    void saveCustomer(Customer customer);
+    void saveCustomer(Customer customer) throws NullPointerException;
 
-    Customer getCustomerByEmailAndPassword(String email, String password) throws AuthenticationException;
+    Customer getCustomerByEmailAndPassword(String email, String password) throws AuthenticationException, InvalidUserNameAndPasswordException;
 
     List<Customer> getAllCustomer();
 
-    void updateCustomer(String email, Customer customer);
+    void updateCustomer(String email, Customer customer) throws InvalidEmailException, NullPointerException;
 
-    void deleteCustomer(long id);
+    void deleteCustomer(long id) throws InvalidIdException, AuthenticationNotFoundException;
 }
