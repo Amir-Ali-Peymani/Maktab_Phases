4dae337136ed9c34ebc31baa8460648ac14e93d2
amrialiPeymani <piliconwalys@gmail.com>
2023-11-07 11:12:25 +0330

    write the ResourceNotFoundException.java class

---
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.phase3.exception;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
