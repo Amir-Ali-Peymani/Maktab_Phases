ff9620e3a42e89f9b7e84de180b2b926a9deeae7
amrialiPeymani <piliconwalys@gmail.com>
2023-11-07 11:15:07 +0330

    write the @Service annotation in the AdminService.java

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/AdminService.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/AdminService.java
@@ -2,9 +2,11 @@ package com.example.phase3.service;
 
 import com.example.phase3.entity.Admin;
 import com.example.phase3.exception.AuthenticationException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface AdminService {
 
     Admin findAdminByEmailAndPassword(String email, String password) throws AuthenticationException;
