2e5eddfd32cd2bb7dc158d5c3e07012323d00dc4
amrialiPeymani <piliconwalys@gmail.com>
2023-11-07 16:45:37 +0330

    made SpecialistController.java class

---
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/controller/SpecialistController.java
@@ -0,0 +1,68 @@
+package com.example.phase3.controller;
+
+
+import com.example.phase3.entity.Specialist;
+import com.example.phase3.exception.*;
+import com.example.phase3.exception.NullPointerException;
+import com.example.phase3.service.SpecialistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/specialist")
+public class SpecialistController {
+
+    private final SpecialistService specialistService;
+
+
+    @Autowired
+    public SpecialistController(SpecialistService specialistService) {
+        this.specialistService = specialistService;
+    }
+
+    @PostMapping("/saveSpecialist")
+    public void saveSpecialist(@RequestBody Specialist specialist) throws NullPointerException {
+        specialistService.saveSpecialist(specialist);
+    }
+
+    @GetMapping("/login/{email}/{password}")
+    public ResponseEntity<?> specialistLogin(@PathVariable("email") String email,
+                                             @PathVariable("password") String password){
+        try{
+            Specialist specialist = specialistService.getCustomerByEmailAndPassword(email, password);
+            return ResponseEntity.ok(specialist);
+        } catch (InvalidUserNameAndPasswordException e) {
+            throw new RuntimeException(e);
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/getAllSpecialist")
+    public ResponseEntity<?> getAllSpecialist(){
+        try {
+            List<Specialist> specialists = specialistService.getAllSpecialists();
+            return ResponseEntity.ok(specialists);
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
+        }
+    }
+
+    @PutMapping("/updateSpecialist/{email}")
+    public void updateSpecialist(@PathVariable("email") String email,
+                                 @RequestBody Specialist specialist) throws NullPointerException, InvalidEmailException {
+        specialistService.updateSpecialist(email, specialist);
+    }
+
+    @DeleteMapping("/deleteSpecialist/{id}")
+    public void deleteSpecialist(@PathVariable("id") Long id) throws AuthenticationNotFoundException {
+        specialistService.deleteSpecialist(id);
+    }
+
+
+
+}
