8e8ccd0c2b46b4e9393b38944d54137108dfebdd
amrialiPeymani <piliconwalys@gmail.com>
2023-11-07 16:39:06 +0330

    edit AdminServiceImpl.java

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/Impl/AdminServiceImpl.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/Impl/AdminServiceImpl.java
@@ -2,9 +2,16 @@ package com.example.phase3.service.Impl;
 
 import com.example.phase3.entity.Admin;
 import com.example.phase3.exception.AuthenticationException;
+import com.example.phase3.exception.AuthenticationNotFoundException;
+import com.example.phase3.exception.InvalidEmailException;
+import com.example.phase3.exception.InvalidUserNameAndPasswordException;
+import com.example.phase3.exception.NullPointerException;
 import com.example.phase3.repository.AdminRepository;
 import com.example.phase3.service.AdminService;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
+import org.hibernate.validator.internal.constraintvalidators.bv.EmailValidator;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,10 +24,14 @@ public class AdminServiceImpl implements AdminService {
 
 
     @Override
-    public Admin findAdminByEmailAndPassword(String email, String password) throws AuthenticationException {
+    public Admin findAdminByEmailAndPassword(@Email String email, String password) throws AuthenticationNotFoundException,
+            InvalidUserNameAndPasswordException {
+        if (email == null || email.equals("") ||  password == null || password.equals("")) {
+            throw new InvalidUserNameAndPasswordException();
+        }
         Admin admin =  adminRepository.findAdminByEmailAndPassword(email, password);
         if (admin == null) {
-            throw new AuthenticationException("Could not find");
+            throw new AuthenticationNotFoundException();
         }
         return admin;
     }
@@ -31,8 +42,14 @@ public class AdminServiceImpl implements AdminService {
     }
 
     @Override
-    public void updateAdmin(String email, Admin admin) {
+    public void updateAdmin(String email, Admin admin) throws InvalidEmailException, NullPointerException {
+        if (email == null || email.equals("")) {
+            throw new InvalidEmailException();
+        }
         Admin adminUpdate = adminRepository.getAdminByEmail(email);
+        if (admin == null) {
+            throw new NullPointerException();
+        }
         adminUpdate.setPassword(admin.getPassword());
         adminUpdate.setEmail(admin.getEmail());
         adminRepository.save(adminUpdate);
