983caa8cf0e3d384040ea27f59b3381a3d6550f5
amrialiPeymani <piliconwalys@gmail.com>
2023-11-07 11:14:27 +0330

    write CustomerController.java class

---
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/controller/CustomerController.java
@@ -0,0 +1,64 @@
+package com.example.phase3.controller;
+
+
+import com.example.phase3.entity.Customer;
+import com.example.phase3.exception.AuthenticationException;
+import com.example.phase3.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping("/saveCustomer")
+    public void saveCustomer(@RequestBody Customer customer) {
+        customerService.saveCustomer(customer);
+    }
+
+
+    @GetMapping("/login/{email}/{password}")
+    public ResponseEntity<?> customerLoginIn(@PathVariable("email") String email,
+                                             @PathVariable("password") String password){
+        try {
+            Customer customer = customerService.getCustomerByEmailAndPassword(email, password);
+            return ResponseEntity.ok(customer);
+        }catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+
+    @GetMapping("/getAllCustomer")
+    public ResponseEntity<?> getAllCustomer(){
+        try {
+            List<Customer> customers = customerService.getAllCustomer();
+            return ResponseEntity.ok(customers);
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
+        }
+    }
+
+    @PutMapping("/updateCustomer/{email}")
+    public void updateCustomer(@PathVariable("email") String email,
+                               @RequestBody Customer customer){
+        customerService.updateCustomer(email, customer);
+    }
+
+    @DeleteMapping("/deleteCustomer/{id}")
+    public void deleteCustomer(@PathVariable("id") Long id){
+        customerService.deleteCustomer(id);
+    }
+
+}
