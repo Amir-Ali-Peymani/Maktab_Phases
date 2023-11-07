e3f0ac1aa43a4a50d2bebb8238bb6979149e5e58
amrialiPeymani <piliconwalys@gmail.com>
2023-11-07 16:47:19 +0330

    added ServiceRepository.java

---
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/repository/ServiceRepository.java
@@ -0,0 +1,11 @@
+package com.example.phase3.repository;
+
+import Phase_Two.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, Long> {
+
+    Service getServiceByName(String name);
+}
