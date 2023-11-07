0f24dc7c84df5c46bd76f2d51eb28991a07a4ae5
amrialiPeymani <piliconwalys@gmail.com>
2023-11-07 16:38:54 +0330

    edit AdminService.java

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/AdminService.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/AdminService.java
@@ -2,6 +2,10 @@ package com.example.phase3.service;
 
 import com.example.phase3.entity.Admin;
 import com.example.phase3.exception.AuthenticationException;
+import com.example.phase3.exception.AuthenticationNotFoundException;
+import com.example.phase3.exception.InvalidEmailException;
+import com.example.phase3.exception.InvalidUserNameAndPasswordException;
+import com.example.phase3.exception.NullPointerException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,9 +13,10 @@ import java.util.List;
 @Service
 public interface AdminService {
 
-    Admin findAdminByEmailAndPassword(String email, String password) throws AuthenticationException;
+    Admin findAdminByEmailAndPassword(String email, String password) throws AuthenticationNotFoundException,
+            InvalidUserNameAndPasswordException;
 
     List<Admin> getAllAdmin();
 
-    void updateAdmin(String email, Admin admin);
+    void updateAdmin(String email, Admin admin) throws InvalidEmailException, NullPointerException;
 }
