ce58827e259828673d0a7cea09e2b7c3009e0e2f
amrialiPeymani <piliconwalys@gmail.com>
2023-11-07 16:44:00 +0330

    added Service.java class in the entity package

---
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/entity/Service.java
@@ -0,0 +1,26 @@
+package com.example.phase3.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String name;
+
+//    @OneToMany(mappedBy = "service")
+//    private Set<SubService> subServices;
+
+}
