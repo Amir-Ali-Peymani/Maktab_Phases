fae4861714615d2ba70448e351e43a40d05ea076
amrialiPeymani <piliconwalys@gmail.com>
2023-11-07 16:43:40 +0330

    make NullPointerException.java

---
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/exception/NullPointerException.java
@@ -0,0 +1,8 @@
+package com.example.phase3.exception;
+
+public class NullPointerException extends Exception{
+
+    public NullPointerException() {
+        super("NullPointerException");
+    }
+}
