6cce70629722ee57dc21483fa7d3fae11a65e79f
amrialiPeymani <piliconwalys@gmail.com>
2023-11-07 16:38:37 +0330

    edit AdminController.java with adding exception

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/controller/AdminController.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/controller/AdminController.java
@@ -2,7 +2,8 @@ package com.example.phase3.controller;
 
 
 import com.example.phase3.entity.Admin;
-import com.example.phase3.exception.AuthenticationException;
+import com.example.phase3.exception.*;
+import com.example.phase3.exception.NullPointerException;
 import com.example.phase3.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,24 +30,19 @@ public class AdminController {
         try {
             Admin admin = adminService.findAdminByEmailAndPassword(email, password);
             return ResponseEntity.ok(admin);
-        } catch (AuthenticationException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (BaseHttpException e) {
+            return ResponseEntity.status(e.getStatus()).body(e.getMessage());
         }
     }
 
     @GetMapping("/getAllAdmin")
-    public ResponseEntity<?> getAllAdmin(){
-        try {
-            List<Admin> adminList = adminService.getAllAdmin();
-            return ResponseEntity.ok(adminList);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred.");
-        }
+    public List<Admin> getAllAdmin(){
+        return adminService.getAllAdmin();
     }
 
     @PutMapping("/updateAdmin/{email}")
     public void updateAdmin(@PathVariable("email") String email,
-                            @RequestBody Admin admin){
+                            @RequestBody Admin admin) throws NullPointerException, InvalidEmailException {
         adminService.updateAdmin(email, admin);
     }
 }
