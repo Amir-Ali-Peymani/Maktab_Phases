62497bc7b955ce557831664627365104bb8942b1
amrialiPeymani <piliconwalys@gmail.com>
2023-11-08 15:06:17 +0330

    made Proposal.java class

---
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/entity/Proposal.java
@@ -0,0 +1,33 @@
+package com.example.phase3.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Proposal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "specialist_id", nullable = false)
+    private Specialist specialist;
+
+    private double proposedPrice;
+
+    private Date startTime;
+
+    private int duration;
+}
