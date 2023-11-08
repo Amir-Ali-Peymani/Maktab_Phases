32f741304d12b8658ac59df2f301d25118019ad6
amrialiPeymani <piliconwalys@gmail.com>
2023-11-08 19:10:23 +0330

    added @Service annotation

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/ProposalService.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/ProposalService.java
@@ -3,9 +3,11 @@ package com.example.phase3.service;
 import com.example.phase3.entity.Proposal;
 import com.example.phase3.exception.AuthenticationNotFoundException;
 import com.example.phase3.exception.NullPointerException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface ProposalService {
 
     void saveProposal(Proposal proposal) throws NullPointerException;
