d55b401985b9b2288d0f164ebd8b522d900a38e4
amrialiPeymani <piliconwalys@gmail.com>
2023-11-08 14:22:43 +0330

    made OrderStatus.java enum

---
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/enumeration/OrderStatus.java
@@ -0,0 +1,15 @@
+package com.example.phase3.enumeration;
+
+
+public enum OrderStatus {
+
+    AWAITING_SPECIALIST_PROPOSAL,
+
+    AWAITING_SPECIALIST_SELECTION,
+
+    STARTED,
+
+    COMPLETED,
+
+    PAID
+}
