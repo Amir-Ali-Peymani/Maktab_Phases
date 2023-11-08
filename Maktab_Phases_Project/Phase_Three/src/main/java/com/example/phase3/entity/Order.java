83c3d777be01c001fdc336a35170390cd6a8b94c
amrialiPeymani <piliconwalys@gmail.com>
2023-11-08 14:21:14 +0330

    made Order.java class

---
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/entity/Order.java
@@ -0,0 +1,59 @@
+package com.example.phase3.entity;
+
+import com.example.phase3.enumeration.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private double proposedPrice;
+
+    private String jobDescription;
+
+    private Date date;
+
+    @Column
+    private double finalPrice;
+
+    @Column
+    private Date compeletionDate;
+
+    @ManyToOne(optional = false
+
+    )
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "specialist_id")
+    private Specialist specialist;
+
+//    @ManyToOne(optional = false
+//    )
+//    @JoinColumn(name = "subService_id")
+//    private SubService subService;
+//
+//    @OneToMany(mappedBy = "order")
+//    private List<Proposal> proposals;
+//
+//    @OneToMany(mappedBy = "order")
+//    private List<Review> reviews;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+}
