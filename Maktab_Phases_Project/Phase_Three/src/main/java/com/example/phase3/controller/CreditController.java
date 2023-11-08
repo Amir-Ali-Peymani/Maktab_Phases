e1d4d7e4ff885bcece8078f999f06263ece2b85d
amrialiPeymani <piliconwalys@gmail.com>
2023-11-08 15:05:12 +0330

    made CreditController.java class

---
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/controller/CreditController.java
@@ -0,0 +1,56 @@
+package com.example.phase3.controller;
+
+import com.example.phase3.entity.Credit;
+import com.example.phase3.exception.AuthenticationNotFoundException;
+import com.example.phase3.exception.BaseHttpException;
+import com.example.phase3.exception.NullPointerException;
+import com.example.phase3.service.CreditService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/credit")
+public class CreditController {
+
+    private final CreditService creditService;
+
+    @PostMapping("/saveCredit")
+    public void saveCredit(@RequestBody Credit credit) throws NullPointerException {
+        creditService.saveCredit(credit);
+    }
+
+    @GetMapping("/getCredit/{id}")
+    public ResponseEntity<?> getCredit(@PathVariable("id") long id){
+        try{
+            Credit credit = creditService.getCreditById(id);
+            return ResponseEntity.ok(credit);
+        }catch (BaseHttpException e) {
+            return ResponseEntity.status(e.getStatus()).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/getAllCredit")
+    public ResponseEntity<?> getAllCredit(){
+        try{
+            List<Credit> credits = creditService.getAllCredit();
+            return ResponseEntity.ok(credits);
+        } catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/updateCredit/{id}")
+    public void updateCredit(@PathVariable("id") long id,@RequestBody Credit credit) throws NullPointerException {
+        creditService.updateCredit(id,credit);
+    }
+
+    @DeleteMapping("/deleteCredit/{id}")
+    public void deleteCredit(@PathVariable("id") long id) throws AuthenticationNotFoundException {
+        creditService.deleteCredit(id);
+    }
+}
