28b629aba4c38130c6b40945b4f2d218bab26387
amrialiPeymani <piliconwalys@gmail.com>
2023-11-08 18:22:00 +0330

    made ProposalController.java class

---
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/controller/ProposalController.java
@@ -0,0 +1,56 @@
+package com.example.phase3.controller;
+
+import com.example.phase3.entity.Proposal;
+import com.example.phase3.exception.AuthenticationNotFoundException;
+import com.example.phase3.exception.BaseHttpException;
+import com.example.phase3.exception.NullPointerException;
+import com.example.phase3.service.ProposalService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/proposal")
+public class ProposalController {
+
+    private final ProposalService proposalService;
+
+    @PostMapping("/saveProposal")
+    public void saveProposal(@RequestBody Proposal proposal) throws NullPointerException {
+        proposalService.saveProposal(proposal);
+    }
+
+    @GetMapping("/getProposal/{id}")
+    public ResponseEntity<?> getProposal(@PathVariable("id")long id){
+        try {
+            Proposal proposal = proposalService.getProposalById(id);
+            return ResponseEntity.ok(proposal);
+        }catch (BaseHttpException e){
+            return ResponseEntity.status(e.getStatus()).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/getAllProposal")
+    public ResponseEntity<?> getAllProposals(){
+        try {
+            List<Proposal> proposals = proposalService.getAllProposals();
+            return ResponseEntity.ok(proposals);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/updateProposal/{id}")
+    public void updateProposal(@PathVariable("id")long id, @RequestBody Proposal proposal) throws NullPointerException {
+        proposalService.updateProposal(id, proposal);
+    }
+
+    @DeleteMapping("/deleteProposal/{id}")
+    public void deleteProposal(@PathVariable("id") long id) throws AuthenticationNotFoundException {
+        proposalService.deleteProposal(id);
+    }
+}
