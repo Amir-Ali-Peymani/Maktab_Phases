51b912be121f280a2a6f921a2ce8b4d28f3af9a6
amrialiPeymani <piliconwalys@gmail.com>
2023-11-08 19:10:15 +0330

    added @Service annotation

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/CreditService.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/CreditService.java
@@ -3,9 +3,11 @@ package com.example.phase3.service;
 import com.example.phase3.entity.Credit;
 import com.example.phase3.exception.AuthenticationNotFoundException;
 import com.example.phase3.exception.NullPointerException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface CreditService {
 
     void saveCredit(Credit credit) throws NullPointerException;
