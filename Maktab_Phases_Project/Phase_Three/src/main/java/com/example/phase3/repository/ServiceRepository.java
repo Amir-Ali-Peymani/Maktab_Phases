011e09366b56f300f958a91d8f107e2b11c96f89
amrialiPeymani <piliconwalys@gmail.com>
2023-11-08 13:10:53 +0330

    added one method

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/repository/ServiceRepository.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/repository/ServiceRepository.java
@@ -1,11 +1,14 @@
 package com.example.phase3.repository;
 
-import Phase_Two.entity.Service;
+
+import com.example.phase3.entity.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ServiceRepository extends JpaRepository<Service, Long> {
+    Service getServicesByName(String name);
 
-    Service getServiceByName(String name);
+    Service getServicesById(Long id);
 }
