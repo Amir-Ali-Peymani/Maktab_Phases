cc38c5ac80a9e3ca41f9e67303c28d4546ffdea9
amrialiPeymani <piliconwalys@gmail.com>
2023-11-08 19:40:06 +0330

    uncomment some lines of code

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/entity/Customer.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/entity/Customer.java
@@ -28,13 +28,13 @@ public class Customer {
     @Column(name= "password", nullable = false)
     private String password;
 
-//    @OneToOne(mappedBy = "customer")
-//    private Credit credit;
-//
-//    @OneToMany(mappedBy = "customer")
-//    private Set<Order> orders;
-//
-//    @OneToMany(mappedBy = "customer")
-//    private Set<Review> reviews;
+    @OneToOne(mappedBy = "customer")
+    private Credit credit;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<Order> orders;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<Review> reviews;
 
 }
