c1691a1fbae41ac916b051bfe8f0d0770dd9ab02
amrialiPeymani <piliconwalys@gmail.com>
2023-11-11 16:43:46 +0330

    created CreditDTO.java class

---
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/dto/CreditDTO.java
@@ -0,0 +1,27 @@
+package com.example.phase3.dto;
+
+import com.example.phase3.entity.Credit;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreditDTO {
+    private long id;
+    private double balance;
+    private long customerId;
+    private long specialistId;
+
+    public static CreditDTO fromCredit(Credit credit){
+        CreditDTO creditDTO = new CreditDTO();
+        creditDTO.setId(credit.getId());
+        creditDTO.setBalance(credit.getBalance());
+        creditDTO.setCustomerId(credit.getCustomer().getId());
+        creditDTO.setSpecialistId(credit.getSpecialist().getId());
+        return creditDTO;
+    }
+}
