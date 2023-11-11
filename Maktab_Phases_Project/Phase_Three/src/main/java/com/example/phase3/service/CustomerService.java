f81bde24a842c2f2d5fa1262da74c960820eae90
amrialiPeymani <piliconwalys@gmail.com>
2023-11-11 16:47:49 +0330

    added method

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/CustomerService.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.phase3.service;
 
+import com.example.phase3.dto.CustomerDTO;
 import com.example.phase3.entity.Customer;
+import com.example.phase3.entity.Order;
 import com.example.phase3.exception.*;
 import com.example.phase3.exception.NullPointerException;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,15 @@ public interface CustomerService {
 
     void saveCustomer(Customer customer) throws NullPointerException;
 
-    Customer getCustomerByEmailAndPassword(String email, String password) throws AuthenticationException, InvalidUserNameAndPasswordException;
+    CustomerDTO getCustomerByEmailAndPassword(String email, String password) throws AuthenticationException, InvalidUserNameAndPasswordException;
 
-    List<Customer> getAllCustomer();
+    List<CustomerDTO> getAllCustomer();
 
     void updateCustomer(String email, Customer customer) throws InvalidEmailException, NullPointerException;
 
     void deleteCustomer(long id) throws InvalidIdException, AuthenticationNotFoundException;
+
+    void giveOrder(long id, long subServiceId, Order order);
+
+    void selectProposal(long proposalId);
 }
