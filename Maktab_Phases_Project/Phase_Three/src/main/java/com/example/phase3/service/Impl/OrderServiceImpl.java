df85d57b930eeb2fd65c3b70278c7e062e5a7011
amrialiPeymani <piliconwalys@gmail.com>
2023-11-11 16:49:56 +0330

    added some method

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/Impl/OrderServiceImpl.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/Impl/OrderServiceImpl.java
@@ -1,9 +1,14 @@
 package com.example.phase3.service.Impl;
 
+import com.example.phase3.dto.OrderDTO;
 import com.example.phase3.entity.Order;
+import com.example.phase3.entity.SubService;
+import com.example.phase3.enumeration.OrderStatus;
 import com.example.phase3.exception.AuthenticationNotFoundException;
 import com.example.phase3.exception.NullPointerException;
+import com.example.phase3.exception.PriceException;
 import com.example.phase3.repository.OrderRepository;
+import com.example.phase3.repository.SubServiceRepository;
 import com.example.phase3.service.OrderService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,26 +21,37 @@ public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
 
+    private final SubServiceRepository subServiceRepository;
+
     @Override
     public void saveOrder(Order order) throws NullPointerException {
         if (order == null || order.getCustomer() == null) {
             throw new NullPointerException();
         }
-        orderRepository.save(order);
+        order.setOrderStatus(OrderStatus.AWAITING_SPECIALIST_PROPOSAL);
+        SubService subService = subServiceRepository.getSubServiceById(order.getSubService().getId());
+        if (order.getProposedPrice() < subService.getBasePrice()){
+            orderRepository.save(order);
+        }else{
+            throw new PriceException();
+        }
     }
 
     @Override
-    public Order getOrderById(Long id) throws AuthenticationNotFoundException {
+    public OrderDTO getOrderById(Long id) throws AuthenticationNotFoundException {
         Order order = orderRepository.getOrderById(id);
         if (order == null) {
             throw new AuthenticationNotFoundException();
         }
-        return order;
+        return OrderDTO.fromOrder(order);
     }
 
     @Override
-    public List<Order> getAllOrder() {
-        return orderRepository.findAll();
+    public List<OrderDTO> getAllOrder() {
+        List<Order> orders = orderRepository.findAll();
+        return orders.stream()
+                .map(OrderDTO::fromOrder)
+                .toList();
     }
 
     @Override
