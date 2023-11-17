0c583b7687b89f4654ca7b71e16bf7d5ce5c6def
amrialiPeymani <piliconwalys@gmail.com>
2023-11-17 12:44:28 +0330

    delete unused imports

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/controller/CreditController.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/controller/CreditController.java
@@ -1,5 +1,6 @@
 package com.example.phase3.controller;
 
+import ch.qos.logback.core.model.Model;
 import com.example.phase3.dto.CreditDTO;
 import com.example.phase3.entity.Credit;
 import com.example.phase3.exception.AuthenticationNotFoundException;
@@ -7,10 +8,17 @@ import com.example.phase3.exception.BaseHttpException;
 import com.example.phase3.exception.NullPointerException;
 import com.example.phase3.service.CreditService;
 import lombok.AllArgsConstructor;
+import lombok.Value;
+import org.springframework.data.jpa.domain.JpaSort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 @AllArgsConstructor
@@ -24,6 +32,17 @@ public class CreditController {
     public void saveCredit(@RequestBody Credit credit) throws NullPointerException {
         creditService.saveCredit(credit);
     }
+
+    @GetMapping(value = "/showFrom", produces = MediaType.TEXT_HTML_VALUE)
+    public String showPaymentForm() throws IOException {
+        return Files.readString(Path.of("D:\\java_projects\\Maktab_Phases\\Maktab_Phases_Project\\Phase_Three\\src\\main\\resources\\templates\\payment.html"));
+    }
+
+    @PostMapping("/process")
+    public String processPayment(@ModelAttribute Credit credit){
+        return "redirect:/payment/success";
+    }
+
 
     @GetMapping("/getCredit/{id}")
     public ResponseEntity<?> getCredit(@PathVariable("id") long id){
