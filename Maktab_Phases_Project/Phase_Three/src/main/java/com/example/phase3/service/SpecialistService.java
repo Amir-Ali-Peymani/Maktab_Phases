3a64fbb8f7562b3e925a7159ebfa700beb2876bf
amrialiPeymani <piliconwalys@gmail.com>
2023-11-17 12:51:06 +0330

    added some library

--- Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/SpecialistService.java
+++ Maktab_Phases_Project/Phase_Three/src/main/java/com/example/phase3/service/SpecialistService.java
@@ -7,7 +7,9 @@ import com.example.phase3.entity.Specialist;
 import com.example.phase3.exception.*;
 import com.example.phase3.exception.NullPointerException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -23,5 +25,9 @@ public interface SpecialistService {
 
     void deleteSpecialist(long id) throws AuthenticationNotFoundException;
 
-    void specialistsGiveProposal(long id, long orderId, Proposal proposal);
+    void specialistsGiveProposal(long id, long orderId, Proposal proposal) throws UnAuthorizedSpecialistException;
+    
+    String uploadImage(long id, MultipartFile file) throws IOException;
+
+    byte[] downloadImage(long id);
 }
